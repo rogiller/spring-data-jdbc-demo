88ce6a82316975966e4b85df5f3745eb5b7a55b2
Roger Diller <roger@rogers-mbp.lan>
2018-11-17 18:44:19 -0500

    initial commit after start.spring.io download

---
+++ src/main/groovy/com/demo/springdatajdbcdemo/SpringDataJdbcDemoApplication.groovy
@@ -0,0 +1,12 @@
+package com.demo.springdatajdbcdemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringDataJdbcDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SpringDataJdbcDemoApplication, args
+	}
+}
---
+++ src/test/groovy/com/demo/springdatajdbcdemo/SpringDataJdbcDemoApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.demo.springdatajdbcdemo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class SpringDataJdbcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
