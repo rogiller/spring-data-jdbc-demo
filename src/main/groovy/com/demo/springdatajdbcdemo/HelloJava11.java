5f5d19fdf46cf1cc71888fc5d897f30b302301b1
Roger Diller <roger@flexrentalsolutions.com>
2018-11-17 19:03:00 -0500

    working Java 11 stuff

---
+++ src/main/groovy/com/demo/springdatajdbcdemo/HelloJava11.java
@@ -0,0 +1,28 @@
+package com.demo.springdatajdbcdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.IntFunction;
+
+@RestController
+public class HelloJava11 {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(required = false, defaultValue = "0") int number){
+
+        //Java 10
+
+        //var
+        var myString = "    Hello from Java 11! ";
+
+        IntFunction<Integer> doubleIt1 = (int x) -> x * 2;
+
+        //Java 11
+        IntFunction<Integer> doubleIt = x -> x * 2;
+
+        return myString.stripTrailing() + " " + doubleIt.apply(number);
+    }
+
+}
